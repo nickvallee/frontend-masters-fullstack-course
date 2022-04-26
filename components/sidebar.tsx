8292337ce0e8244325fe194ee486549eb744bad6
Nick Vallee <nickvallee@gmail.com>
2022-04-26 15:34:34 -0400

    initial commit

---
+++ components/sidebar.tsx
@@ -0,0 +1,24 @@
+import {
+  Center,
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  LinkBox,
+  LinkOverlay,
+} from "@chakra-ui/layout";
+
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+const Sidebar = () => {
+  return <div>hello from sidebar</div>;
+};
+
+export default Sidebar;
