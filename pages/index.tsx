4a5d3bf699015672cb41cdd4a0c67fa9c2ac1e29
Nick Vallee <nickvallee@gmail.com>
2022-06-01 12:42:59 -0400

    a nother commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,17 @@ import GradientLayout from "../components/gradientLayout";
 import prisma from "../lib/prisma";
 import { Box, Flex, Text } from "@chakra-ui/layout";
 import { Image } from "@chakra-ui/react";
+import { useMe } from "../lib/hooks";
 
 export default function Home({ artists }) {
-  // console.log("ARtISTS", artists);
+  const { user } = useMe();
+
   return (
     <GradientLayout
       color="green"
       subtitle="profile"
-      title="Nick Vallee"
-      description="15 public playlists"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user.playlistsCount} public playlists`}
       roundImage={true}
       image="https://tinted-gym-f99.notion.site/image/https%3A%2F%2Fdl.dropboxusercontent.com%2Fs%2Fbgiv0ssz3xpotz9%2Fpeep.png%3Fdl%3D0?table=block&id=33f9771b-0e6f-4a72-832c-69ed2d41f290&spaceId=511cd811-5561-4a61-b550-c4086b4afafb&width=2000&userId=&cache=v2"
     >
